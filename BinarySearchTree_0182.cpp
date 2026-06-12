5b0653055b77e48946ae0305cb8cbfeec7cba908
“BANTENGSELAM” <pedjuangjava@gmail.com>
2026-06-12 15:04:40 +0700

    membuat class node

--- BinarySearchTree_0182.cpp
+++ BinarySearchTree_0182.cpp
@@ -4,4 +4,11 @@ using namespace std; // library
 //membuat classs node
 class node {
 
+    //memberi akses modifier serta mendeklarasi variable dan object node
+    public :
+    string info;
+    Node* lecfChild;
+    Node* rightChild;
+
+    
 };
